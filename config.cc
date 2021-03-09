06d42175207602ca9295861d85dc9cfeab3bb277
Fredrik Lind <fredrik.lind.96@gmail.com>
2021-03-09 18:23:35 +0100

    Added DB init functions

--- config.cc
+++ config.cc
@@ -35,10 +35,46 @@ void create_default_config(fs::path& stream)
 	cfg.writeFile(stream.c_str());
 }
 
+int init_pads(sqlite3*& db, char*& err)
+{
+	// Of course this shouldn't be hardcoded
+	// But I have to draw the line somewhere
+	sqlite3_exec(db, 
+			"CREATE TABLE IF NOT EXISTS 'pads' ("
+			"floor INTEGER NOT NULL,"
+			"number INTEGER NOT NULL,"
+			"max_weight REAL NOT NULL,"
+			"PRIMARY KEY (floor, number));", 
+			nullptr, 
+			nullptr,
+			&err);
+
+	return (err == nullptr) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+int init_ships(sqlite3*& db, char*& err)
+{
+	sqlite3_exec(db,
+			"CREATE TABLE IF NOT EXISTS 'ships' ("
+			"ship_id INTEGER PRIMARY KEY NOT NULL,"
+			"license TEXT NOT NULL,"
+			"manufacturer TEXT,"
+			"weight REAL NOT NULL,"
+			"pad INTEGER NOT NULL,"
+			"FOREIGN KEY (pad) REFERENCES 'pads'"
+			"ON DELETE CASCADE ON UPDATE CASCADE);",
+			nullptr,
+			nullptr,
+			&err);
+
+	return (err == nullptr) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
 int main(int argc, char* argv[])
 {
 
 	Config cfg;
+	sqlite3* db;
 
 	fs::path config_path = fs::current_path().append("config.cfg");
 	fs::path db_path;
@@ -74,29 +110,51 @@ int main(int argc, char* argv[])
 		}
 	}
 
+	// Open the database -- a new one will be created if none exists so 
+	// this shouldn't fail unless an I/O error occurs,
+	// or the path leads to a non-empty non-DB file.
+	// Either way we can fail here.
+	if (sqlite3_open(db_path.c_str(), &db))
+	{
+		fprintf(stderr, "Failed to open database: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return EXIT_FAILURE;
+	}
+	
 	for (int index = optind; index < argc; index++)
 	{
 		if (strcmp(argv[index], "default") == 0)
 		{
 			create_default_config(config_path);
 			fprintf(stdout, "Default configuration created at %s\n", config_path.c_str());
-			return EXIT_SUCCESS;
 		}
-
-		if (strcmp(argv[index], "init") == 0)
+		else if (strcmp(argv[index], "init") == 0)
 		{
-			fprintf(stdout, "Creating new database...\n");
-			return EXIT_SUCCESS;
-		}
+			char* err;
+			int errc = 0;
+			if (init_pads(db, err))
+			{
+				fprintf(stderr, "Failed to init pads table: %s\n", err);
+				sqlite3_free(err);
+				errc++;
+			}
+			if (init_ships(db, err))
+			{
+				fprintf(stderr, "Failed to init ships table: %s\n", err);
+				sqlite3_free(err);
+				errc++;
+			}
 
-		if (strcmp(argv[index], "delete") == 0)
+			fprintf(stdout, (errc == 0) ? 
+					"Database initialized successfully!\n" : "%i error(s) occurred.\n", errc);
+		}
+		else if (strcmp(argv[index], "delete") == 0)
 		{
 			fprintf(stdout, "Deleting database...\n");
 		}
-		else
+		else 
 		{
 			fprintf(stderr, "Unknown operation '%s'!\n", argv[index]);
-			return EXIT_FAILURE;
 		}
 
 		no_input = false;
@@ -105,5 +163,6 @@ int main(int argc, char* argv[])
 	if (no_input)
 		print_usage();
 	
+	sqlite3_close(db);
 	return EXIT_SUCCESS;
 } 
