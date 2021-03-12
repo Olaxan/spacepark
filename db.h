14933928e9f56c12aac4efd8ccbfc057dfef32c7
Fredrik Lind <fredrik.lind.96@gmail.com>
2021-03-12 14:34:13 +0100

    Minor changes.

--- db.h
+++ db.h
@@ -1,3 +1,27 @@
+/*
+ * This file is part of SPACEPARK.
+ *
+ * Developed for the VISMA graduate program code challenge.
+ * See the COPYRIGHT file at the top-level directory of this distribution
+ * for details of code ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * If issues occur, contact me on fredrik.lind.96@gmail.com
+ *
+ */
+
 #pragma once
 
 #include <sqlite3.h>
--- parksrv.h
+++ parksrv.h
@@ -17,6 +17,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * If issues occur, contact me on fredrik.lind.96@gmail.com
+ *
  */
 
 #pragma once
